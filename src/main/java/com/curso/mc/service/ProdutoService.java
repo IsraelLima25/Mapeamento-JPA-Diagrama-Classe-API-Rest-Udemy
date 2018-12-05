5718f57a46567cb79027d6a371d7c9b38da7def4
DESKTOP-BQKTILS\israe <israel_lima_16@hotmail.com>
2018-12-04 23:33:26 -0200

    produto e Associacao muitos para muitos

---
+++ src/main/java/com/curso/mc/service/ProdutoService.java
@@ -0,0 +1,16 @@
+package com.curso.mc.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.curso.mc.repositories.ProdutoRepository;
+
+@Service
+public class ProdutoService {
+	
+	@Autowired
+	private ProdutoRepository repo;
+	
+	
+
+}
