72a115055b0a4c7aababeece50690bb826419f10
DESKTOP-BQKTILS\israe <israel_lima_16@hotmail.com>
2018-12-12 11:38:01 -0200

    Inserindo uma nova Categoria com Post

--- src/main/java/com/curso/mc/resources/CategoriaResource.java
+++ src/main/java/com/curso/mc/resources/CategoriaResource.java
@@ -1,11 +1,15 @@
 package com.curso.mc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.curso.mc.domain.Categoria;
 import com.curso.mc.service.CategoriaService;
@@ -21,13 +25,20 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaService service;
 
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	/*
 	 * O tipo ResponsEntity(Spring) já encapsula uma série de informações da
 	 * resposta http para um serviço rest.
 	 */
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
-		Categoria obj = service.buscar(id);		
+		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok(obj);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
