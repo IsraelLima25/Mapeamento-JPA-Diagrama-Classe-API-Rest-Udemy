ee477ed5737c012e8e4cf07903d04ac0b9476097
DESKTOP-BQKTILS\israe <israel_lima_16@hotmail.com>
2018-12-14 13:56:05 -0200

    Busca de produtos por nome e categorias

--- src/main/java/com/curso/mc/service/ProdutoService.java
+++ src/main/java/com/curso/mc/service/ProdutoService.java
@@ -1,19 +1,32 @@
 package com.curso.mc.service;
 
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
+import java.util.List;
 
-//import com.curso.mc.repositories.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
 
-//@Service
+import com.curso.mc.domain.Categoria;
+import com.curso.mc.domain.Produto;
+import com.curso.mc.repositories.CategoriaRepository;
+import com.curso.mc.repositories.ProdutoRepository;
+
+
+@Service
 public class ProdutoService {
 	
-	/*
-	 * 	@Autowired
-	private ProdutoRepository repo;
-	 */
+	@Autowired
+	private ProdutoRepository prodRepo;
+	@Autowired
+	private CategoriaRepository catRepo;
 
-	
-	
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy,
+			String direction) {
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		List<Categoria> categorias = catRepo.findAllById(ids);		
+		return prodRepo.search(nome, categorias, pageRequest);
+	}
 
 }
